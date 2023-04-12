bb0b9e5d26fa0b133768dc4562c8ccc35eefe67d
Rancho91 <ramirosanchezsolano@gmail.com>
2023-04-12 19:10:44 -0300

    arreglar ruta

--- src/handlers/genreHandler.js
+++ src/handlers/genreHandler.js
@@ -3,14 +3,13 @@ const {Genregames} = require('../db')
 const genreHandler = async (req,res) => {
 
     try {
-        console.log('ingreso la ruta')
         listGenre = await Genregames.findAll()
-
         res.status(200).json(listGenre)
     } catch (error) {
         res.status(400).json({error: "no se encontraron dietas"})
     }
 }
+
 
 module.exports = {genreHandler}
 
