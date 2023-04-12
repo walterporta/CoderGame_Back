1f545c18af78f7c3561b5b8a797fc9378338f2f8
Rancho91 <ramirosanchezsolano@gmail.com>
2023-04-12 15:47:39 -0300

    ModelUser en db

--- src/db.js
+++ src/db.js
@@ -5,6 +5,7 @@ const {
  } = process.env;
 const modelVideoGames = require('./models/VideoGame.js')
 const modelGenreGames = require('./models/GenreGame.js')
+const modelUsers = require('./models/User.js')
 
 const db = new Sequelize(
    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/codergame`,
@@ -15,6 +16,7 @@ const db = new Sequelize(
 
  modelVideoGames(db);
  modelGenreGames(db);
+ modelUsers(db);
 
 modelVideoGames.belongsToMany(modelGenreGames, { through: 'GameGenre'})// mucho a muchos, tabla intermedia
 modelGenreGames.belongsToMany(modelVideoGames, { through: 'GameGenre'})// tiene que tener mismo nombre
