f5f8f2c87f36146146e5b322ed5e6aa6f5271cbd
Rancho91 <ramirosanchezsolano@gmail.com>
2023-04-12 19:27:01 -0300

    Controller findGameName

---
+++ src/controllers/findGameName.js
@@ -0,0 +1,13 @@
+const {Videogame} = require('../db')
+
+const findGameName = (name) =>{
+
+    const findGame = Videogame.findAll({
+        where: {
+            name: {[Op.iLike]: `%${name}%`}
+        }
+    })
+    return findGame
+}
+
+module.exports = {findGameName}
