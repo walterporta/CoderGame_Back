d213333726c26e2f69491e27a7708e3f717fccd8
Rancho91 <ramirosanchezsolano@gmail.com>
2023-04-14 06:58:26 -0300

    filtro convinado terminado

--- src/controllers/findGameName.js
+++ src/controllers/findGameName.js
@@ -5,62 +5,32 @@ const { Op, where } = require('sequelize')
 
 const findGameName = async (name, genre) => {
 
-
     let findGame = []
-    if (!name) {
-        findGame = await Videogames.findAll({
-            include: [{
-                model: Genregames,
-                attributes: ['id', 'name'],
-                through: {
-                    attributes: []
-                } // nombre del campo que quieres recuperar
-            }],
-            where: {
-                deleted: false
-            }
-        })
 
-    } else {
-        findGame = await Videogames.findAll({
-            where: {
-                name: { [Op.iLike]: `%${name}%` },
-                delete: false
-            },
-            include: [{
-                model: Genregames,
-                attributes: ['id', 'name'],
-                through: {
-                    attributes: []
-                } // nombre del campo que quieres recuperar
-            }]
-        })
-    }
-
-    let findGames = await Videogames.findAll({
+     findGame = await Videogames.findAll({
         where:{
             [Op.and]:[name?{name: {[Op.iLike]: `%${name}%`}}:null ],
-            delete: false  
+            deleted: false,
+        
         },
         include: [{
             model: Genregames,
             attributes: ['id', 'name'],
             where: {
                 name: genre ? genre : { [Op.ne]: null,
-                },
-                delete: false,
+                }
             },
                 
             through: {
                 attributes: []
             },
-            required: false 
+            required: true 
         }]
     })
 
-     console.log(findGames)
+     console.log(findGame)
     
-    return findGames
+    return findGame
 }
 
 
