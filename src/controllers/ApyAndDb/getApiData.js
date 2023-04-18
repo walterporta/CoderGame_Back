74de1e6b281adcde04da62a54a9a6532a6a9a627
walterporta <walterandresporta@gmail.com>
2023-04-18 17:06:53 -0300

    trae con imagenes

--- src/controllers/ApyAndDb/getApiData.js
+++ src/controllers/ApyAndDb/getApiData.js
@@ -4,30 +4,32 @@ const axios = require('axios');
 
 //Indo de la Api 
 let searchApi = async () => {
-    let arrVideogames = []
-  for(let i = 1; i < 30; i++){ 
-    try {
-        const apiUrl = await axios.get(`https://api.rawg.io/api/games/${i}?key=${API_KEY}`);
+    let arrVideogames = [];
+    for (let i = 1; i < 200; i++) {
+      try {
+        const apiUrl = await axios.get(
+          `https://api.rawg.io/api/games/${i}?key=${API_KEY}`
+        );
         arrVideogames.push(apiUrl.data);
-      } catch (error) {
-      }
-   arrVideogames = arrVideogames.map(el=>{
-    return{
-        name: el.name? el.name: null,
-         image: el.background_image? el.background_image:null,
-         released: el.released?el.released:null,
-         rating: el.rating?el.rating:null,
-        platforms: el.platforms,
-         description: el.description?el.description:null,
-         genres: el.genres?el.genres:null,
-         price: Math.floor(Math.random() * (1000 - 500 + 1) + 500)
+      } catch (error) {}
     }
-   })
-    }
-    arrVideogames = arrVideogames.filter((game)=>game.description !== null)
-return arrVideogames;
+    arrVideogames = arrVideogames
+      .map((el) => {
+        return {
+          name: el.name ? el.name : null,
+          image: el.background_image ? el.background_image : null,
+          released: el.released ? el.released : null,
+          rating: el.rating ? el.rating : null,
+          platforms: el.platforms,
+          description: el.description ? el.description : null,
+          genres: el.genres ? el.genres : null,
+          price: Math.floor(Math.random() * (1000 - 500 + 1) + 500),
+        };
+      })
+      .filter((game) => game.description !== null && game.image !== null);
+    return arrVideogames;
+  };
 
- }
 
 //Db
 const searchDB = async () => {
