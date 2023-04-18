f9839db9573be319f536c70ca3163c50b25d74aa
walterporta <walterandresporta@gmail.com>
2023-04-18 15:16:37 -0300

    wallet

--- src/models/Wallet.js
+++ src/models/Wallet.js
@@ -1,24 +1,21 @@
-// const { DataTypes } = require("sequelize");
+const { DataTypes } = require("sequelize");
 
-// module.exports = (sequelize) => {
-//     sequelize.define(
+module.exports = (sequelize) => {
+    sequelize.define(
 
-//     'Wallet',
-//     {
-//       id: {
-//         type: DataTypes.UUID,
-//         defaultValue: DataTypes.UUIDV4,
-//         allowNull: false,
-//         primaryKey: true,
-//       },
-//       balance: {
-//         type: DataTypes.FLOAT,
-//         allowNull: false,
-//         defaultValue: 0,
-//       },
-//     },
-//     { timestamps: false }
-//   )}
-
-
-// SIN TERMINAR
+    'Wallet',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      balance: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
+      },
+    },
+    { timestamps: false }
+  )}
