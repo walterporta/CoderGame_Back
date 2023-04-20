fbee580302c2ea4bfc319cb3f278a4ea95c644d5
Rancho91 <ramirosanchezsolano@gmail.com>
2023-04-20 18:29:36 -0300

    controllers y rutas del compra y carga de saldo

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ const videoGamesRoutes = require('./videoGamesRoutes');
 const platformsRoutes = require('./platformsRoutes')
 const usersRoutes = require('./usersRoutes.js')
 const dbLoad = require('./dbLoad')
-const pagoRoute = require('./pagoRoute')
 const checkout = require('./coRoute');
 const router = Router();
 
@@ -14,7 +13,6 @@ router.use('/videogames', videoGamesRoutes);
 router.use('/platforms', platformsRoutes);
 router.use('/cargadb', dbLoad)
 router.use('/user', usersRoutes);
-router.use('/pagos', pagoRoute)
 router.use('/checkout', checkout)
 
 module.exports = router;
