f4a3e97a55b52456dee82a8a09019cf6ffe02880
Disroderick <roderickrodriguez706@gmail.com>
2023-04-20 17:11:58 -0500

    cam

--- src/models/User.js
+++ src/models/User.js
@@ -4,26 +4,13 @@ module.exports = (sequelize) => {
   sequelize.define(
     'Users',
     {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
+      auth0Id: {
+        type: DataTypes.TEXT,
         allowNull: false,
         primaryKey: true,
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
         unique: true
       },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
       name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      lastname: {
         type: DataTypes.STRING,
         allowNull: false
       },
@@ -32,21 +19,6 @@ module.exports = (sequelize) => {
         allowNull: false,
         unique: true,
       },
-      gender: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        isIn: [['male', 'female', 'other']]
-      },
-    //  Esto al final va a ser un ENUM, con 3 roles, user,empresa y admin
-    // isAdmin: {
-    //     type: DataTypes.BOOLEAN,
-    //     defaultValue: false
-    //   },
-    //   isSuperAdmin: {
-    //     type: DataTypes.BOOLEAN,
-    //     defaultValue: false
-    //   }
-      
     },
     {
       timestamps: false,
