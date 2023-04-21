1da113404a0ad12bcd0073a6cd25a13f0d9ef0c4
Disroderick <roderickrodriguez706@gmail.com>
2023-04-21 11:16:11 -0500

    funciona 10pto

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,15 +1,15 @@
 const { Router } = require('express')
-const { deleteFavoriteHandler, createUserHandler, addFavoriteHandler, getFavoriteHandler} = require('../handlers/usersHandlers')
+const { deleteFavoriteHandler, createUserHandler, addFavoriteHandler, getFavoriteHandler } = require('../handlers/usersHandlers')
 
 
 const usersRoutes = Router()
 
 // usersRoutes.get('/', getUserHandler)
 // usersRoutes.get('/:id', getUserByIdHandler)
-usersRoutes.post('/', createUserHandler)
+usersRoutes.post('/register', createUserHandler)
 usersRoutes.post('/favorites', addFavoriteHandler)
-usersRoutes.delete('/favorites', deleteFavoriteHandler) 
+usersRoutes.delete('/favorites', deleteFavoriteHandler)
 usersRoutes.get('/favorites/:id', getFavoriteHandler)
 
 module.exports = usersRoutes
- 
+
