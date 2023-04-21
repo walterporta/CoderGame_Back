7eb973eeabb8562e3bb18186a25ddb7cede9d6e2
Rancho91 <ramirosanchezsolano@gmail.com>
2023-04-21 18:43:56 -0300

    pull

--- src/controllers/Users/usersControllers.js
+++ src/controllers/Users/usersControllers.js
@@ -1,11 +1,6 @@
 const { Users } = require('../../db.js')
 //const bcrypt = require('bcryptjs')
 
-<<<<<<< HEAD
-const createNewUser = async ({id, name, nickname, email }) => {
-   
-    const newUser = await Users.create({ id, name, nickname, email })
-=======
 const createNewUser = async ({ sub, name, email }) => {
     // if (!sub) {
     //     throw new Error('El valor de sub es inválido')
@@ -24,7 +19,6 @@ const createNewUser = async ({ sub, name, email }) => {
     })
     if (created) console.log('creado');
     else console.log('ya existe');
->>>>>>> edbe74cadc5f51ce1c086718542a735f2cbb12f0
     return newUser
 }
 
