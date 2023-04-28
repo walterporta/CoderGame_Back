2db7be8d52d0d79629a0da59d5eb59ebc1f19b90
Rancho91 <ramirosanchezsolano@gmail.com>
2023-04-28 08:55:42 -0300

    add filtro promotions a findGame

--- src/handlers/adminHandler.js
+++ src/handlers/adminHandler.js
@@ -5,12 +5,11 @@ const getAllUsersHandler = async (req,res)=>{
     const {sub} = req.body
   
     try { 
-    
         const response = await getAllUsers(sub)
         res.status(200).json(response)
         
     } catch (error) {
-        res.status(400).json(error)
+        res.status(400).json(error.message)
     }
 }
 
