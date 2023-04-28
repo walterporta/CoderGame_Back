8eef4abd9ab950df0d6c79c9d3d1b2b4caaf5f4b
Rancho91 <ramirosanchezsolano@gmail.com>
2023-04-28 16:41:27 -0300

    checkout

--- src/controllers/Admin/banUser.js
+++ src/controllers/Admin/banUser.js
@@ -3,10 +3,9 @@ const {Users}=require('../../db')
 const banUser = async (subUser, subAdmin) =>{
     let ban
     const findUser = await Users.findOne({where:{sub:subUser}})
-    console.log(findUser)
     
     findUser.banned? ban=false: ban=true
-    
+
     const user = await Users.update({banned:ban},{where:{sub:subUser}})
 
     return user
