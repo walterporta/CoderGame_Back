08690584ec05900f3e2a43f503f9a54e504e4acf
walterporta <walterandresporta@gmail.com>
2023-04-28 20:43:41 -0300

    anda lo de fer

---
+++ src/controllers/Users/getUserPurchases.js
@@ -0,0 +1,45 @@
+const { Favorites, Videogames, ComentariosV, Wallets, Users } = require('../../db');
+
+const getUserPurchases = async (sub) => {
+  try {
+    console.log(sub)
+    const favorites = await Favorites.findAll({
+      where: {
+        UserSub: sub, // Corrected from 'id' to 'sub'
+        buy: true,
+      },
+      include: [{
+        model: Videogames,
+        attributes: ['id', 'name', 'image', 'gameLink'],
+        include: [{
+          model: ComentariosV,
+          include: [{
+            model: Users,
+            attributes: ['name']
+          }]
+        }]
+      }]
+    });
+
+    const wallet = await Wallets.findOne({
+      where: {
+        UserSub: sub
+      },
+      attributes: ['balance']
+    });
+
+    return {
+      favorites: favorites,
+      balance: wallet.balance
+    };
+
+  } catch (error) {
+    console.error(error);
+    return {
+      message: 'Server error'
+    };
+  }
+};
+
+
+module.exports = { getUserPurchases }
