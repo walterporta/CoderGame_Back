a7d09924a269dfa157e23293df1d368214e965af
Rancho91 <ramirosanchezsolano@gmail.com>
2023-04-30 17:07:28 -0300

    correcciones en deleteYbanned

--- src/controllers/Admin/banDelUser.js
+++ src/controllers/Admin/banDelUser.js
@@ -2,7 +2,8 @@ const {Users}=require('../../db')
 const verifyRol = require('../../helpers/verifyRol')
 
 const banDelUser = async (subUser, subAdmin, banDelet) =>{
-    const rol = verifyRol(subAdmin)
+    const rol = await verifyRol(subAdmin)
+    console.log(rol)
     if(rol !== 'admin') throw new Error('you must be admin')
 
     let bannedDeleted
@@ -10,7 +11,7 @@ const banDelUser = async (subUser, subAdmin, banDelet) =>{
     
     findUser[banDelet]? bannedDeleted=false: bannedDeleted=true
     
-    const user = await Users.update({[banDelet]:ban},{where:{sub:subUser}})
+    const user = await Users.update({[banDelet]:bannedDeleted},{where:{sub:subUser}})
 
     return user
 }
