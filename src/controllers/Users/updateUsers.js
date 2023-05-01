c5afd1fc92aee5ffdd55a7632406bcd92ba6e0fa
walterporta <walterandresporta@gmail.com>
2023-05-01 17:13:38 -0300

    agregue delete a put user

--- src/controllers/Users/updateUsers.js
+++ src/controllers/Users/updateUsers.js
@@ -1,42 +1,45 @@
-const { Users } = require('../../db')
+const { Users } = require('../../db');
 
-const updateUsers = async (sub, { name, rol, banned }) => {
-    
-    if (!sub || typeof sub !== 'string') {
-      throw new Error('Se requiere un ID de usuario válido');
+const updateUsers = async (sub, { name, rol, banned, deleted }) => {
+  if (!sub || typeof sub !== 'string') {
+    throw new Error('Se requiere un ID de usuario válido');
+  }
+
+  const user = await Users.findByPk(sub);
+  if (!user) {
+    throw new Error('No se encontró ningún usuario con el SUB proporcionado');
+  }
+
+  if (name) {
+    if (typeof name !== 'string' || name.trim() === '') {
+      throw new Error('El nombre no es válido');
     }
-  
-   
-    const user = await Users.findByPk(sub);
-    if (!user) {
-      throw new Error('No se encontró ningún usuario con el SUB proporcionado');
+    user.name = name.trim();
+  }
+
+  if (rol) {
+    if (rol !== 'admin' && rol !== 'client' && rol !== 'seller') {
+      throw new Error('El rol no es válido');
     }
-  
-    
-    if (name) {
-      if (typeof name !== 'string' || name.trim() === '') {
-        throw new Error('El nombre no es válido');
-      }
-      user.name = name.trim();
+    user.rol = rol;
+  }
+
+  if (banned !== undefined) {
+    if (typeof banned !== 'boolean') {
+      throw new Error('El valor de banned no es válido');
     }
-    if (rol) {
-      if (rol !== 'admin' && rol !== 'client' && rol !== 'seller') {
-        throw new Error('El rol no es válido');
-      }
-      user.rol = rol;
+    user.banned = banned;
+  }
+
+  if (deleted !== undefined) {
+    if (typeof deleted !== 'boolean') {
+      throw new Error('El valor de deleted no es válido');
     }
-    if (banned !== undefined) {
-      if (typeof banned !== 'boolean') {
-        throw new Error('El valor de banned no es válido');
-      }
-      user.banned = banned;
-    }
-  
-        
-    const updatedUser = await user.save();
-  
-    
-    return updatedUser;
-  };
-  
-  module.exports = { updateUsers }
+    user.deleted = deleted;
+  }
+
+  const updatedUser = await user.save();
+  return updatedUser;
+};
+
+module.exports = { updateUsers };
