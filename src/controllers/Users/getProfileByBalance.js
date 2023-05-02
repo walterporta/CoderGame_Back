e6f6ec6ab9633cd433bd1de7a0759d65c3b300d8
walterporta <walterandresporta@gmail.com>
2023-05-02 16:51:24 -0300

    sumamos name al getprofile

--- src/controllers/Users/getProfileByBalance.js
+++ src/controllers/Users/getProfileByBalance.js
@@ -1,14 +1,21 @@
-const { Profile, Wallets } = require('../../db')
+const { Profile, Wallets, Users } = require("../../db");
 
 const getProfileByBalance = async (sub) => {
   try {
-    const userProfile = await Profile.findOne({ where: { UserSub: sub } })
-    const userBalance = await Wallets.findOne({ where: { UserSub: sub }, attributes: ['balance'] })
+    const userProfile = await Profile.findOne({
+      where: { UserSub: sub },
+      include: { model: Users, attributes: ["name"] },
+    });
 
-    return { profile: userProfile, balance: userBalance }
+    const userBalance = await Wallets.findOne({
+      where: { UserSub: sub },
+      attributes: ["balance"],
+    });
+
+    return { profile: userProfile, balance: userBalance };
   } catch (error) {
-    throw new Error(`Error al cargar el perfil y saldo: ${error.message}`)
+    throw new Error(`Error al cargar el perfil y saldo: ${error.message}`);
   }
-}
+};
 
-module.exports = { getProfileByBalance }
+module.exports = { getProfileByBalance };
